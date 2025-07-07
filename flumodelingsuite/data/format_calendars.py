6ec5c4861a41ea90127e3eb33f01f20149112a98
rlewinter <remy.lewinter@gmail.com>
2025-07-07 18:28:45 -0400

    initial working package

---
+++ flumodelingsuite/data/format_calendars.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+### This script cleans our school closure data and saves it in a consistent format.
+
+# read the uncleaned data
+school_calendars = pd.read_csv('school_calendars_corrected.csv', parse_dates=['start','end'])
+
+# normalize names
+school_calendars.event_name = school_calendars.event_name.str.removesuffix('\t').str.strip().str.title()
+
+# NOTE: One state, Virginia, has a Rosh Hashanah break. This may actually be representative of the state, as 
+# the break exists in at least Fairfax County, Arlington County, and Richmond. This creates a couple problems.
+# First, if treating US school holidays as the sum of all state holidays, this will be misrepresentative.
+# Second, Rosh Hashanah is irregular. We might be able to handle this like Thanksgiving, but for now I'll
+# just exclude Rosh Hashanah.
+school_calendars = school_calendars[school_calendars.event_name != 'Rosh Hashanah Break']
+
+# NOTE: Three states have an Easter break. Since Easter is an irregular holiday I'll also drop it for now.
+school_calendars = school_calendars[school_calendars.event_name != 'Easter Break']
+
+# NOTE: Federal and state holidays will be added by the functions for adding school closure interventions to models
+
+# create summer breaks from first/last day of school fields
+school_calendars = school_calendars.sort_values(['state','start']).drop(columns=['NCES_district_id','name'])
+
+summer_breaks = school_calendars[school_calendars.event_name.isin(['First Day Of School','Last Day Of School'])].pivot(
+    columns='event_name', index='state', values='start').reset_index().rename(columns={'Last Day Of School':'start','First Day Of School':'end'})
+summer_breaks['event_name'] = 'Summer Break'
+
+school_calendars = pd.concat([school_calendars,summer_breaks]).dropna(ignore_index=True)
+
+# store only the day and month of closures
+school_calendars['start_day'] = school_calendars.start.dt.day
+school_calendars['start_month'] = school_calendars.start.dt.month
+school_calendars['end_day'] = school_calendars.end.dt.day
+school_calendars['end_month'] = school_calendars.end.dt.month
+school_calendars.drop(columns=['start','end'], inplace=True)
+
+# save to csv
+school_calendars.to_csv('school_calendars_formatted.csv', index=False)
---
+++ flumodelingsuite/flumodelingsuite/seasonality.py
@@ -0,0 +1,128 @@
+### seasonality.py
+# Functions for generating seasonal transmission rates.
+import datetime as dt
+from typing import Optional, Callable
+
+def _calc_seasonality_balcan_at_t(t: int, t_max: int, R_min: float, R_max: float, period: int = 365) -> float:
+    """
+    Calculate the seasonal transmission rate based on the time of year. Period can be customized.
+    Implementation of eq25 from https://www.sciencedirect.com/science/article/pii/S1877750310000438
+
+    Parameters:
+        t (int): Day of year (0-365).
+        t_max (int): Day of year when the transmission rate is at its maximum.
+        R_min (float): The minimum transmission rate.
+        R_max (float): The maximum transmission rate.
+        period (int): The period of the seasonality in days (default=365).
+
+    Returns:
+        float: The seasonal transmission rate at time t.
+    """
+    import numpy as np
+    
+    return ( (1 - (R_min / R_max)) * np.sin( (2 * np.pi / period) * (t - t_max) + (np.pi / 2) ) + 1 + (R_min/R_max) ) / 2
+
+
+def calc_seasonality_balcan_at_date(
+    date_t: dt.date,
+    date_start: dt.date,
+    date_tmax: dt.date,
+    R_min: float,
+    R_max: float,
+    date_tmin: Optional[dt.date] = None,
+    period: Optional[int] = None
+) -> float:
+    """
+    Computes the seasonal rate for a given date using the Balcan model.
+
+    Parameters:
+        date_t: Target date.
+        date_start: Reference start date where t=0.
+        date_tmax: Date when the transmission rate is at its maximum.
+        date_tmin: Date when the transmission rate is at its minimum (optional).
+        R_min: The minimum transmission rate.
+        R_max: The maximum transmission rate.
+        period: The period of the seasonality in days (default=365). If None, derives using date_tmin and date_start or defaults to 365.
+
+    Returns:
+        Seasonality factor at date_t.
+    """
+
+    t_days = (date_t - date_start).days
+    t_max_days = (date_tmax - date_start).days
+
+    if period is not None:
+        p = period
+    elif date_tmin is not None:
+        t_min_days = (date_tmin - date_start).days
+        p = 2 * abs(t_min_days - t_max_days)
+    else:
+        p = 365
+        
+    return _calc_seasonality_balcan_at_t(t_days, t_max_days, R_min, R_max, p)
+
+
+def generate_seasonal_values(
+    date_start: dt.date,
+    date_stop: dt.date,
+    seasonality_func: Callable[[dt.date], float]
+) -> tuple[list[dt.date], list[float]]:
+    """
+    A generic function to generate values over a date range using any seasonality function.
+
+    Parameters:
+        date_start: Start date.
+        date_stop: End date.
+        seasonality_func: A function that computes the seasonal rate for a given date (e.g. calc_seasonality_balcan_at_date).
+
+    Returns:
+        Tuple of (dates, values).
+    """
+    import datetime as dt
+    
+    dates = [date_start + dt.timedelta(days=i) for i in range((date_stop - date_start).days + 1)]
+    values = [seasonality_func(d) for d in dates]
+    return dates, values
+
+
+def get_seasonal_transmission_balcan(
+    date_start: dt.date,
+    date_stop: dt.date,
+    date_tmax: dt.date,
+    R_min: float,
+    R_max: float,
+    date_tmin: Optional[dt.date] = None
+) -> tuple[list[dt.date], list[float]]:
+    '''
+    Returns seasonal transmission rates for the specified simulation period using the Balcan model.
+    This is a wrapper for calc_seasonality_balcan_at_date() and generate_seasonal_values().
+    
+    Parameters:
+        date_start: Reference start date where t=0 (start of simulation period).
+        date_stop: End date.
+        date_tmax: Date when the transmission rate is at its maximum.
+        date_tmin: Date when the transmission rate is at its minimum (optional).
+        R_min: The minimum transmission rate.
+        R_max: The maximum transmission rate.
+
+    Returns:
+        Tuple of (dates, values).
+    '''
+    from functools import partial
+
+    balcan_calculator = partial(
+        calc_seasonality_balcan_at_date,
+        date_start=date_start,
+        date_tmax=date_tmax,
+        date_tmin=date_tmin,
+        R_min=R_min,
+        R_max=R_max
+    )
+
+    dates, values = generate_seasonal_values(
+        date_start=date_start,
+        date_stop=date_stop,
+        seasonality_func=balcan_calculator
+    )
+
+    return dates, values
