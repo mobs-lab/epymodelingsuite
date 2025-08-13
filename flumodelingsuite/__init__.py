a0e61ba37b94a1ec82f86ff40350cdc231c22a03
rlewinter <remy.lewinter@gmail.com>
2025-08-12 22:00:56 -0400

    place imports in init file

--- flumodelingsuite/__init__.py
+++ flumodelingsuite/__init__.py
@@ -1,1 +1,18 @@
 # flumodelingsuite/__init__.py
+
+from .vaccinations import scenario_to_epydemix, smh_data_to_epydemix, make_vaccination_probability_function, add_vaccination_schedule
+from .seasonality import get_seasonal_transmission_balcan
+from .school_closures import make_school_closure_dict, add_school_closure_interventions
+from .config_loader import load_model_config_from_file, setup_epimodel_from_config
+
+__all__ = [
+    'scenario_to_epydemix',
+    'smh_data_to_epydemix',
+    'make_vaccination_probability_function',
+    'add_vaccination_schedule',
+    'get_seasonal_transmission_balcan',
+    'make_school_closure_dict',
+    'add_school_closure_interventions',
+    'load_model_config_from_file',
+    'setup_epimodel_from_config'
+]
