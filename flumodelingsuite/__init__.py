7ec399132e288c7e4cf35cb8c226befeaa6feac7
Minami Ueda <minami.ueda@gmail.com>
2025-09-17 23:16:05 -0400

    Add function to load configs

--- flumodelingsuite/__init__.py
+++ flumodelingsuite/__init__.py
@@ -1,6 +1,10 @@
 # flumodelingsuite/__init__.py
 
-from .config_loader import load_model_config_from_file, setup_epimodel_from_config
+from .config_loader import (
+    load_basemodel_config_from_file,
+    load_calibration_config_from_file,
+    load_sampling_config_from_file,
+)
 from .school_closures import add_school_closure_interventions, make_school_closure_dict
 from .seasonality import get_seasonal_transmission_balcan
 from .vaccinations import (
@@ -14,7 +18,9 @@ __all__ = [
     "add_school_closure_interventions",
     "add_vaccination_schedule",
     "get_seasonal_transmission_balcan",
-    "load_model_config_from_file",
+    "load_basemodel_config_from_file",
+    "load_calibration_config_from_file",
+    "load_sampling_config_from_file",
     "make_school_closure_dict",
     "make_vaccination_probability_function",
     "scenario_to_epydemix",
