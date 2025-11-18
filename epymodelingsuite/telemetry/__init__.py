b3cfe0dc4223b39351eb4471ef3a7d465e5ef1e0
Minami Ueda <minami.ueda@gmail.com>
2025-11-17 20:42:27 -0500

    Fix import error: extract_builder_metadata from extractors module

--- epymodelingsuite/telemetry/__init__.py
+++ epymodelingsuite/telemetry/__init__.py
@@ -3,8 +3,8 @@
 from .core import (
     ExecutionTelemetry,
     create_workflow_telemetry,
-    extract_builder_metadata,
 )
+from .extractors import extract_builder_metadata
 
 __all__ = [
     "ExecutionTelemetry",
