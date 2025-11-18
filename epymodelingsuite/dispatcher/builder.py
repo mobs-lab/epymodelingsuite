4562dbcd0ad2f2e9ee00666d1fea6fcfe490db61
nmoghaddas19 <102733763+nmoghaddas19@users.noreply.github.com>
2025-11-18 10:41:26 -0500

    Update builder.py

--- epymodelingsuite/dispatcher/builder.py
+++ epymodelingsuite/dispatcher/builder.py
@@ -340,7 +340,9 @@ def build_calibration(
         sampled_start_timespan = None
 
     # Vaccination is sensitive to location and start_date but not to model parameters.
-    models, earliest_vax = setup_vaccination_schedules(basemodel, models, sampled_start_timespan, population_names.tolist())
+    if not isinstance(population_names, list):
+        population_names = population_names.tolist()
+    models, earliest_vax = setup_vaccination_schedules(basemodel, models, sampled_start_timespan, population_names)
 
     # These interventions are sensitive to location but not to model parameters and can be applied
     # using the earliest start_date before creating ABCSamplers.
