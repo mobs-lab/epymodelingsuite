296e4bac12ec2c50f9e9b39bcb50ebed050650d6
rlewinter <remy.lewinter@gmail.com>
2025-11-19 16:50:14 -0500

    create example files

---
+++ tutorials/data/udf_examples.py
@@ -0,0 +1,18 @@
+## udf_examples.py
+# Dummy examples for User-Defined Functions
+import copy
+
+from epydemix.calibration import rmse
+from epydemix.model.simulation_output import Trajectory
+
+
+def udf_distance(data: Dict, simulation: Dict):
+    """Dummy wrapper for epydemix rmse."""
+    return rmse(data, simulation)
+
+
+def udf_transform(trajectory: Trajectory):
+    """Dummy post-hoc transformation function that records a new compartment."""
+    traj = copy.deepcopy(trajectory)
+    traj.compartments["Dummy_L+I"] = traj.compartments["L"] + traj.compartments["I"]
+    return traj
