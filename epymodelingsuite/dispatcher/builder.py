ea68d93e6b80c12c5a26fb2499e9b797a7a4927f
Minami Ueda <minami.ueda@gmail.com>
2026-01-15 19:39:26 -0500

    Declare intervention_types

--- epymodelingsuite/dispatcher/builder.py
+++ epymodelingsuite/dispatcher/builder.py
@@ -288,6 +288,7 @@ def build_sampling(
         sampled_vars = generate_samples(sampling_config, basemodel.random_seed)
 
     # Extract intervention types
+    intervention_types = []
     if basemodel.interventions:
         intervention_types = [i.type for i in basemodel.interventions]
 
