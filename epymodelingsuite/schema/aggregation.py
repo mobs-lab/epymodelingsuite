a2133f68522b62bed8d18c60a9f98869b0b214a8
rlewinter <remy.lewinter@gmail.com>
2026-01-16 15:03:10 -0500

    complete first implementation of aggregation schema

--- epymodelingsuite/schema/aggregation.py
+++ epymodelingsuite/schema/aggregation.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 
 from pydantic import BaseModel, Field
 
@@ -12,25 +13,49 @@ logger = logging.getLogger(__name__)
 
 
 class SourceExperiment(BaseModel):
-    """Source experiment specification for aggregation."""
+    """
+    Source experiment specification for aggregation.
 
-    exp_id: str
-    run_id: str = "latest"
-    weight: float = 1.0  # For weighted aggregation (future)
+    Experiment identifiers for integration with an external pipeline, e.g. epycloud.
+    Used to locate sets of CalibrationOutput objects for aggregation.
+    """
+
+    exp_id: str = Field(description="Experiment ID.")
+    run_id: str = Field("latest", description="Run ID, default 'latest'.")
+    weight: float = Field(1.0, description="")  # For weighted aggregation (future)
+
+
+class AggregationStrategyEnum(str, Enum):
+    """
+    Strategy for aggregating multistrain results.
+    """
+
+    sum = "sum"
+    weighted_sum = "weighted_sum"
+    bootstrap = "bootstrap"
+    correlated = "correlated"
 
 
 class AggregationConfiguration(BaseModel):
     """Configuration for aggregating multiple experiment results."""
 
     meta: Meta | None = Field(None, description="General metadata.")
-    sources: list[SourceExperiment]
-    method: str = (
-        "sum"  # "sum", "bootstrap", "correlated", etc. Not fully sure at the moment but probably something like this.
+    sources: list[SourceExperiment] = Field(
+        description="Identifiers for locating CalibrationOutput objects for aggregation."
     )
-    compartments: list[str] | str = "all"
-    transitions: list[str] | str = "all"
-    method_options: dict = {}  # Method-specific options (e.g., n_samples for bootstrap)
-    output_config: str
+    method: AggregationStrategyEnum = Field(description="Strategy for aggregating multistrain results.")
+    compartments: list[str] | bool = Field(
+        False,
+        description="Aggregate results for compartments. Set `True` to get all compartments, or provide a list of identifiers (e.g. 'I_total') to select compartments.",
+    )
+    transitions: list[str] | bool = Field(
+        False,
+        description="Aggregate results for transitions. Set `True` to get all transitions, or provide a list of identifiers (e.g. 'I_to_R_total') to select transitions.",
+    )
+    method_options: dict = Field(
+        default_factory=dict, description="Method-specific options (e.g., n_samples for bootstrap)."
+    )
+    output_config: str = Field(description="Filename for output configuration.")
 
 
 class AggregationConfig(BaseModel):
